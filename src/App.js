d61e7575c1638ced5e825c0a20a7aeb89f9f5ef9
gabriel <gabrielsil20177@gmail.com>
2025-06-13 13:21:47 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div className="w-[1440px] px-28 py-5 bg-slate-900 inline-flex flex-col justify-start items-center gap-16 overflow-hidden">
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Sobre</a>
+          </li>
+          <li>
+            <a href="#">Projetos</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+      </nav>
+
+      <h1>Gabriel Martins</h1>
+      <h2>Desenvolvedor Web</h2>
+      <p>Dedicado em criar softwares eficientes</p>
+
+      <button>Baixar curriculo</button>
+    </div>
+  );
+}
+
+export default App;
